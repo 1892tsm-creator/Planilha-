dc08dfa707bf2846623633af2aaf55126acc4389
1892tsm-creator <1892tsm@gmail.com>
2025-07-30 12:11:31 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,19 @@
-const CACHE_NAME = 'adornecasa-pwa-v3';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-72x72.png',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
+const CACHE_NAME = 'adornecasa-v4';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  // Adicione outros assets necessários
 ];
 
-// Instalação
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
+      .then(cache => cache.addAll(ASSETS))
       .then(() => self.skipWaiting())
   );
 });
 
-// Ativação
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => key !== CACHE_NAME && caches.delete(key))
-    )).then(() => self.clients.claim())
-  );
-});
-
-// Estratégia: Cache First, fallback para rede
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request)
